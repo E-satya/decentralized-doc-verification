e8fa86a630740c8b827755c538bd601450eb0d32
Pratiksharai-rumsan <pratiksha.rai@rumsan.net>
2024-09-13 20:29:31 +0545

    call the function of contract

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,31 @@
 "use client";
-import { ChainId, MediaRenderer, ThirdwebProvider } from "@thirdweb-dev/react";
+import {
+  ChainId,
+  MediaRenderer,
+  ThirdwebProvider,
+  ThirdwebSDK,
+} from "@thirdweb-dev/react";
 import styles from "./page.module.css";
-import { useStorageUpload, useContract } from "@thirdweb-dev/react";
+import {
+  useStorageUpload,
+  useContract,
+  useContractWrite,
+  Web3Button,
+} from "@thirdweb-dev/react";
 import { useState } from "react";
+import { abi } from "./constant/abi";
+import { sep } from "path";
 
 export default function Home() {
   const { mutateAsync: upload } = useStorageUpload();
   const { contract } = useContract(
-    "0x01a1c045175bDA62F1E246a028353251e0541f45"
+    "0x01a1c045175bDA62F1E246a028353251e0541f45",
+    abi
+  );
+
+  const { mutateAsync, isLoading, error } = useContractWrite(
+    contract,
+    "uploadDocument"
   );
 
   const [file, setFile] = useState(null);
@@ -18,11 +36,12 @@ export default function Home() {
       try {
         console.log("Uploading file:", file);
         const uris = await uploadFile(file);
+        console.log(uris, "uris");
         if (uris && uris.length > 0) {
           const ipfsHash = uris[0];
           console.log(ipfsHash, "hash");
           setUploadedURI(ipfsHash);
-         // await storeHashInContract(ipfsHash);
+          // await storeHashInContract(ipfsHash);
         }
       } catch (error) {
         console.error("Error uploading file:", error);
@@ -36,6 +55,7 @@ export default function Home() {
         try {
           const dataToUpload = new Uint8Array(reader.result as ArrayBuffer);
           const uris = await upload({ data: [dataToUpload] });
+          console.log("File uploaded successfully:", uris);
           resolve(uris);
         } catch (error) {
           reject(error);
@@ -58,9 +78,10 @@ export default function Home() {
       const hash = ipfsHash.replace(/^ipfs:\/\//, "").split("/")[0];
       console.log(hash, "hash");
       // @ts-ignore
-      //const tx = await contract.uploadDocument(hash);
+
       // await tx.wait(); // Wait for transaction to be mined
       console.log("Hash stored in contract successfully");
+      const tx = await contract.abi;
     } catch (error) {
       console.error("Error storing hash in contract:", error);
     }
@@ -87,9 +108,29 @@ export default function Home() {
             onChange={handleFileChange}
             className={styles.fileInput}
           />
-          <button className={styles.primaryButton} onClick={handleUploadClick}>
+          {/* <button className={styles.primaryButton} onClick={handleUploadClick}>
             Document upload
+          </button> */}
+          <button className={styles.primaryButton} onClick={handleUploadClick}>
+            Upload Document
           </button>
+          <Web3Button
+            contractAddress={"0x01a1c045175bDA62F1E246a028353251e0541f45"}
+            // Calls the "setName" function on your smart contract with "My Name" as the first argument
+            action={async () => {
+              if (uploadedURI) {
+                const ipfsHash = uploadedURI
+                  .replace(/^ipfs:\/\//, "")
+                  .split("/")[0];
+                console.log(ipfsHash, "hash");
+                await mutateAsync({ args: [ipfsHash] });
+              } else {
+                console.error("No uploaded document URI found");
+              }
+            }}
+          >
+            Send Transaction
+          </Web3Button>
           <button
             className={styles.secondaryButton}
             //onClick={handleVerifyClick}
